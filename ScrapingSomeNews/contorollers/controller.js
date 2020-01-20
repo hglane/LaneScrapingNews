2c6244dccf48ca64013f34410e17f8e1f5e205b4
Harris <hglane@crimson.ua.edu>
2020-01-20 10:52:58 -0600

    finishing comments page

--- ScrapingSomeNews/contorollers/controller.js
+++ ScrapingSomeNews/contorollers/controller.js
@@ -85,4 +85,85 @@ app.get("/stories-json", function (req, res) {
     });
 });
 
+app.get("/clearAll", function(req, res) {
+    Article.remove({}, function(err, doc) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("removed all stories");
+        }
+    });
+    res.redirect("/articles-json");
+});
+
+app.get("/readStory/:id", function(req, res) {
+    var articleId = req.params.id;
+    var hbsObj = {
+        artile: [],
+        body: []
+    };
+
+    Story.findOne({ _id: storyId })
+        .populate("comment")
+        .exec(function(err, doc) {
+            if (err) {
+                console.log("Error: " + err);
+            } else {
+                hbsObj.article = doc;
+                var link = doc.link;
+                request(link, function(error, response, html) {
+                    var $ = cheerio.load(html);
+
+                    $(".1col__main").each(function(i, element) {
+                        hbsObj.body = $(this)
+                        .children("p-featured-content__body")
+                        .children("p")
+                        .text();
+
+                        res.render("article", hbsObj);
+                        return false;
+                    });
+                });
+                
+            }
+        });
+});
+
+app.post("/comment/:id", function(req, res){
+    var user = req.body.name;
+    var content = req.body.comment;
+    var storyId = req.params.id; 
+
+    var commentObj = {
+        name: user, 
+        body: content
+    };
+
+    var newComment = new Comment(commentObj);
+
+    newComment.save(function(err, doc) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(doc._id);
+            console.log(articleId);
+
+            Story.findOneAndUpdate(
+                { _id: req.params.id },
+                { $push: { comment: doc._id } },
+                { new: true }
+            ).exec(function(err, doc){
+                if (err) {
+                    console.log(err);
+                } else {
+                    res.redirect("/readStory/" + storyId);
+                }
+            })
+            
+        }
+    });
+});
+
+
+
 module.exports = app;
